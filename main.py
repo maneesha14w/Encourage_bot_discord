dfad84fa3bf83fd9cb2d8827756dcdcd1644a7b9
Maneesha Walisundera <maneesha14w@gmail.com>
2021-02-03 04:51:31 +0000

    added functionality to add and delete encouragements.

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ def getQuote():
   return (quote)
   
 
+def updateEncouragements(enouraging_msg):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(enouraging_msg)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [enouraging_msg]
+
+
+def del_Encouragements(index):
+  encouragements = db["enouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+    
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user} '.format(client))
